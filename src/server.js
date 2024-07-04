0c75680d8d7aee79f7e48bd4debd13c8d7fee67d
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-07-04 17:02:58 +0700

    initiate server file

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+async function init() {
+  const server = Hapi.server({
+    port: process.env.NODE_ENV !== 'production' ? 8000 : 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
