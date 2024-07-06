e09a1fd5e61a475887cc0e503a3a4ba1722c303f
yosia.manurun@gmail.com <yosia.manurun@gmail.com>
2024-07-06 21:48:14 +0700

    add "addBookHandler" handler

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 const {
   addBookHandler,
-  getAllBooksHandler,
-  getBookByIdHandler,
-  editBookByIdHandler,
-  deleteBookByIdHandler
+  // getAllBooksHandler,
+  // getBookByIdHandler,
+  // editBookByIdHandler,
+  // deleteBookByIdHandler
 } = require('./handler')
 
 const routes = [
@@ -12,26 +12,26 @@ const routes = [
     path: '/books',
     handler: addBookHandler,
   },
-  {
-    method: 'GET',
-    path: '/books',
-    handler: getAllBooksHandler,
-  },
-  {
-    method: 'GET',
-    path: '/books/{bookId}',
-    handler: getBookByIdHandler,
-  },
-  {
-    method: 'PUT',
-    path: '/books/{bookId}',
-    handler: editBookByIdHandler,
-  },
-  {
-    method: 'DELETE',
-    path: '/books/{bookId}',
-    handler: deleteBookByIdHandler,
-  },
+  // {
+  //   method: 'GET',
+  //   path: '/books',
+  //   handler: getAllBooksHandler,
+  // },
+  // {
+  //   method: 'GET',
+  //   path: '/books/{bookId}',
+  //   handler: getBookByIdHandler,
+  // },
+  // {
+  //   method: 'PUT',
+  //   path: '/books/{bookId}',
+  //   handler: editBookByIdHandler,
+  // },
+  // {
+  //   method: 'DELETE',
+  //   path: '/books/{bookId}',
+  //   handler: deleteBookByIdHandler,
+  // },
 ]
 
 module.exports = routes;
